c6eae8d2fca064b2ef6cd87ef18f9618f64a3843
John Sawyer <sawyer@sawyermerchant.com>
2016-11-11 15:57:12 -0500

    Start build_tree

--- lib/DOMparser.rb
+++ lib/DOMparser.rb
@@ -12,6 +12,22 @@ class DOMparser
   end
 
 
+  def build_tree
+    depth = 0
+    until @queue.empty?
+      node = @queue.shift
+      if open_tag?(node)
+        puts "#{'  ' * depth} #{node}"
+        depth += 1
+      elsif close_tag?(node)
+        depth -= 1
+        puts "#{'  ' * depth} #{node}"
+      else
+        puts "#{'  ' * depth} #{node}"
+      end
+    end
+  end
+
 
 end
 
