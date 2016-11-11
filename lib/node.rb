5567e5f42fa81d786e445b90b17d1936c0f1ce78
John Sawyer <sawyer@sawyermerchant.com>
2016-11-11 14:08:01 -0500

    Add Type test

--- lib/node.rb
+++ lib/node.rb
@@ -1,12 +1,22 @@
 class Node
 
-  attr_accessor :children
+  attr_accessor :children, :type
 
-  def initialize(tag)
-    @tag = tag
+  TYPE = /^(<)(\w+)\b/
+  ATTRIBUTES = /(\s\w+)="(\w+[^"]*)/
+
+  def initialize(tag, depth)
+    @type = type(tag)
     @parent = nil
     @children = []
+    @attributes = {}
+    @depth = depth
   end
+
+  def type(tag)
+    tag.match(TYPE)[2] if tag.match(TYPE)
+  end
+
 
 
 end
---
+++ spec/node_spec.rb
@@ -0,0 +1,19 @@
+require 'node'
+
+describe "Node" do
+
+  let(:open_tag) {Node.new("<h1>", 0)}
+  let(:text_tag) {Node.new("I am text", 1)}
+  let(:close_tag) {Node.new("</li", 2)}
+  let(:open_tag_with_attributes) {Node.new("<div class=\"front right\" id=\"main-area\"", 3)}
+  describe "#type" do
+
+    it 'takes a tag and return a type' do
+      expect(open_tag.type).to eq("h1")
+    end
+
+  end
+
+
+
+end
