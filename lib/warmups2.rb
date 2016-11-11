f3574047b16192f6a7ad3f65f50d81f72fe00734
John Sawyer <sawyer@sawyermerchant.com>
2016-11-10 20:01:22 -0500

    Figure out or

--- lib/warmups2.rb
+++ lib/warmups2.rb
@@ -4,16 +4,20 @@ class PageParser
 
   attr_reader :html_string
 
+  # TYPE = /^(<)(\w+)\b/
+  OPEN = /<\w+>/
+  CLOSE = /<\/\w+>/
+  TEXT = /\>\s?(.*?)\s?</
+
+  stack = []
+
   def initialize html_string
-    @data = {}
+    @dom = []
     @html_string = html_string
   end
-  #
 
-  def store
-    type_value = Tag.new(html_string)
-    tag = Hash.new(type:type_value)
+  def craw_text
+    scan(OPEN || CLOSE || TEXT)
   end
-
 
 end
--- spec/warmups2_spec.rb
+++ spec/warmups2_spec.rb
@@ -1,16 +1,31 @@
 require 'warmups2'
 
-describe "PageParser" do 
+describe "PageParser" do
 
   let(:str) {  "<div> div text before <p> p text </p> <div> more div text </div> div text after</div>" }
-  let(:test) {PageParser.new(str)}
+  let(:test_page) {PageParser.new(str)}
 
-  describe "#initialize" do 
-    it "accepts an html string" do 
-      expect(test.html_string).to eq(str)
+  let(:test_dom) { [
+    {type: div, depth: 0, children: []},
+    {type: text, depth: 1, value: "div text before", children[]},
+    {type: p, depth: 1}
+
+
+
+  ]  }
+
+  describe "#initialize" do
+    it "accepts an html string" do
+      expect(test_page.html_string).to eq(str)
     end
 
+  describe "#tag_finder" do
 
+    it 'returns position of first open tag' do
+      expect(test.tag_finder)
+    end
+
+  end
 
 
 
