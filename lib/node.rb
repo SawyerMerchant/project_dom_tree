4f9e4ba1e74cfd06b7036a1583c456404a8d91c1
John Sawyer <sawyer@sawyermerchant.com>
2016-11-11 14:42:22 -0500

    Add attributes to tag

--- lib/node.rb
+++ lib/node.rb
@@ -1,22 +1,40 @@
 class Node
 
-  attr_accessor :children, :type
+  attr_accessor :children, :type, :attributes
 
   TYPE = /^(<)(\w+)\b/
   ATTRIBUTES = /(\s\w+)="(\w+[^"]*)/
 
   def initialize(tag, depth)
-    @type = type(tag)
+    @tag = tag
     @parent = nil
     @children = []
     @attributes = {}
     @depth = depth
+    run
   end
 
-  def type(tag)
-    tag.match(TYPE)[2] if tag.match(TYPE)
+  def run
+    add_type
+    add_attributes
   end
+
+  def add_type
+    if @tag.match(TYPE)
+      @type = @tag.match(TYPE)[2]
+    end
+  end
+
+  def add_attributes
+    scanned = @tag.scan(ATTRIBUTES)
+    scanned.each do |item|
+      @attributes[item[0].strip.to_sym] = item[1].split
+    end
+  end
+
+
 
 
 
 end
+
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,18 +3,29 @@ require 'node'
 describe "Node" do
 
   let(:open_tag) {Node.new("<h1>", 0)}
+
   let(:text_tag) {Node.new("I am text", 1)}
+
   let(:close_tag) {Node.new("</li", 2)}
+
   let(:open_tag_with_attributes) {Node.new("<div class=\"front right\" id=\"main-area\"", 3)}
 
   describe "#type" do
 
     it 'takes a tag and return a type' do
+      open_tag.add_type
       expect(open_tag.type).to eq("h1")
     end
 
   end
 
+  describe "#atributes" do
+
+    it 'takes a tag and returns an attribute hash' do
+      expect(open_tag_with_attributes.attributes).to eq({class: ["front", "right"], id: ["main-area"]})
+    end
+
+  end
 
 
 end
