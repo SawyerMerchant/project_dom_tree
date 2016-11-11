852f5eeffa5c2a95ea5f2e68ef8340910a1b89ff
John Sawyer <sawyer@sawyermerchant.com>
2016-11-11 09:20:38 -0500

    Replace individual regexes with single

--- lib/warmups2.rb
+++ lib/warmups2.rb
@@ -4,24 +4,17 @@ class PageParser
 
   attr_reader :html_string
 
-  # TYPE = /^(<)(\w+)\b/
-  # OPEN = /<\w+>/
-  # CLOSE = /<\/\w+>/
-  # TEXT = /\>\s?(.*?)\s?</
+  TAGS_AND_TEXT = /(<[\/!]?[\w\s="'-]+>\s*)|([^<>]*)/
 
   VOID_ELEMENTS = "area, base, br, col, command, embed, hr, img, input, keygen, link, meta, param, source, track, wbr".split(', ')
-
-
-  stack = []
 
   def initialize html_string
     @dom = []
     @html_string = html_string
+    @stack = []
   end
 
-  def craw_text
-    scan(OPEN || CLOSE || TEXT)
-  end
+
 
 
 
